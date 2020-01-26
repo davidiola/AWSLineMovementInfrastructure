7665e85c94c956cdff49174500b45fb4de69140a
David Iola <iola2@illinois.edu>
2020-01-25 22:30:03 -0800

    adding EC2 instance definition

--- upload_template_s3.sh
+++ upload_template_s3.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-zip cfn-template.zip base-cfn-template.yaml test-stack-configuration.json prod-stack-configuration.json
-aws s3api put-object --bucket aws-line-movement-infrastructure --key cfn-synthesize-resources/cfn-template.zip --body cfn-template.zip
-aws s3api put-object --bucket aws-line-movement-infrastructure --key pipeline/pipeline.yml --body pipeline.yml
+if aws cloudformation validate-template --template-body file://base-cfn-template.yaml
+then
+  zip cfn-template.zip base-cfn-template.yaml test-stack-configuration.json prod-stack-configuration.json
+  aws s3api put-object --bucket aws-line-movement-infrastructure --key cfn-synthesize-resources/cfn-template.zip --body cfn-template.zip
+  aws s3api put-object --bucket aws-line-movement-infrastructure --key pipeline/pipeline.yml --body pipeline.yml
+  rm cfn-template.zip
+else
+  echo "CFN Validation failed"
+fi
 
